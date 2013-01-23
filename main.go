2ae2a066a0beef2bd1255fe474ec1bfc1c2eecbb
Don More <don@zogg.org>
2013-01-23 09:22:15 +0000

    Now thread safe.

--- main.go
+++ main.go
@@ -13,12 +13,14 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 )
 
 var font *truetype.Font
 var imageTime int64
-var imageBytes bytes.Buffer
+var imageBytes []byte
+var imageMutex sync.Mutex
 
 func setup() {
 	// Read the font data.
@@ -38,65 +40,74 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
-func generateImage() {
-	// Remove existing image file.
-	_ = os.Remove("pic.jpeg")
+func getImage() []byte {
+	imageMutex.Lock()
+	defer imageMutex.Unlock()
 
-	// Generate new image.
-	cmd := exec.Command("/usr/bin/streamer", "-c", "/dev/video0", "-o", "pic.jpeg", "-s", "640x360", "-j", "90")
-	err := cmd.Run()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// Open the image file.
-	file, err := os.Open("pic.jpeg")
-	if err != nil {
-		log.Panic(err)
-	}
-	defer file.Close()
-
-	// Decode the image.
-	src, _, err := image.Decode(file)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// Time
 	now := time.Now()
-	imageTime = now.Unix()
-	label := now.Format("2/1/2006 3:04PM")
+	if now.Unix() > imageTime+60 {
+		log.Println("Generating Image")
 
-	// Colour of text
-	fg := image.NewUniform(color.RGBA{255, 255, 0, 255})
+		// Remove existing image file.
+		_ = os.Remove("pic.jpeg")
 
-	// Copy in original image
-	rgba := image.NewRGBA(src.Bounds())
-	draw.Draw(rgba, rgba.Bounds(), src, image.ZP, draw.Src)
+		// Generate new image.
+		cmd := exec.Command("/usr/bin/streamer", "-c", "/dev/video0", "-o", "pic.jpeg", "-s", "640x360", "-j", "90")
+		err := cmd.Run()
+		if err != nil {
+			log.Panic(err)
+		}
 
-	// Draw the text.
-	size := 24.0
-	c := freetype.NewContext()
-	c.SetDPI(72)
-	c.SetFont(font)
-	c.SetFontSize(size)
-	c.SetClip(rgba.Bounds())
-	c.SetDst(rgba)
-	c.SetSrc(fg)
-	pt := freetype.Pt(10, 10+int(c.PointToFix32(size)>>8))
-	_, err = c.DrawString(label, pt)
-	if err != nil {
-		log.Panic(err)
-		return
+		// Open the image file.
+		file, err := os.Open("pic.jpeg")
+		if err != nil {
+			log.Panic(err)
+		}
+		defer file.Close()
+
+		// Decode the image.
+		src, _, err := image.Decode(file)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// Time
+		label := now.Format("2/1/2006 3:04PM")
+
+		// Colour of text
+		fg := image.NewUniform(color.RGBA{255, 255, 0, 255})
+
+		// Copy in original image
+		rgba := image.NewRGBA(src.Bounds())
+		draw.Draw(rgba, rgba.Bounds(), src, image.ZP, draw.Src)
+
+		// Draw the text.
+		size := 24.0
+		c := freetype.NewContext()
+		c.SetDPI(72)
+		c.SetFont(font)
+		c.SetFontSize(size)
+		c.SetClip(rgba.Bounds())
+		c.SetDst(rgba)
+		c.SetSrc(fg)
+		pt := freetype.Pt(10, 10+int(c.PointToFix32(size)>>8))
+		_, err = c.DrawString(label, pt)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// Produce jpeg of new image.
+		options := jpeg.Options{90}
+		newImage := new(bytes.Buffer)
+		err = jpeg.Encode(newImage, rgba, &options)
+		if err != nil {
+			log.Panic(err)
+		}
+		imageTime = now.Unix()
+		imageBytes = newImage.Bytes()
+
 	}
-
-	// Produce jpeg of new image.
-	options := jpeg.Options{90}
-	imageBytes.Reset()
-	err = jpeg.Encode(&imageBytes, rgba, &options)
-	if err != nil {
-		log.Panic(err)
-	}
+	return imageBytes
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -106,11 +117,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// This is not thread safe - access to imageBytes and imageTime
-	if time.Now().Unix() > imageTime+60 {
-		log.Println("Generating Image")
-		generateImage()
-	}
-
-	w.Write(imageBytes.Bytes())
+	w.Write(getImage())
 }
